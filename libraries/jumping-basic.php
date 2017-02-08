a34553760b176aaa1a633cd1f8254792a3e18bf7
gzp <gzp@gzpblog.com>
2017-02-09 02:03:25 +0800

    完善首页,文章页样式,comments.php

--- libraries/jumping-basic.php
+++ libraries/jumping-basic.php
@@ -344,3 +344,90 @@ function jumping_pagenavi( $space = 5 ) {
         printf( '<li><a class="page-numbers" href="%s" title="%s" aria-label="Next"><span aria-hidden="true">%s</span></a></li>', esc_html( get_pagenum_link( $paged + 1 ) ), 'Next »', '»' );
     }
 }
+
+/**
+ * 评论回调函数
+ * @param $comment
+ * @param $args
+ * @param $depth
+ */
+function jumping_comment( $comment, $args, $depth ) {
+    $GLOBALS['comment'] = $comment;
+
+    global $commentcount;
+
+    if ( ! $commentcount ) {
+        $page         = ( ! empty( $in_comment_loop ) ) ? get_query_var( 'cpage' ) - 1 : get_page_of_comment( $comment->comment_ID, $args ) - 1;
+        $cpp          = get_option( 'comments_per_page' );
+        $commentcount = $cpp * $page;
+    }
+
+    if ( ! $comment->comment_parent ) {
+        //$email  = $comment->comment_author_email;
+        $avatar = get_avatar( $comment, $size = '50' );
+        ?>
+        <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+        <div id="comment-<?php comment_ID(); ?>" class="comment-body">
+            <div class="comment-avatar"><?php echo $avatar; ?></div>
+					<span class="comment-floor"><?php ++ $commentcount;
+                        switch ( $commentcount ) {
+                            case 1:
+                                _e( 'Sofa', MUTHEME_NAME );
+                                break;
+                            case 2:
+                                _e( 'Bench', MUTHEME_NAME );
+                                break;
+                            case 3:
+                                _e( 'Floor', MUTHEME_NAME );
+                                break;
+                            default:
+                                printf( __( '%s Floor', MUTHEME_NAME ), $commentcount );
+                        } ?></span>
+
+            <div class="comment-data">
+				<span class="comment-span <?php if ( $comment->user_id == 1 ) {
+                    echo "comment-author";
+                } ?>"><?php printf( '%s', get_comment_author_link() ) ?></span>
+				<span
+                    class="comment-span comment-date"><?php echo strtotime($comment->comment_date_gmt); ?></span>
+            </div>
+            <div class="comment-text"><?php comment_text() ?></div>
+            <div class="comment-reply"><?php comment_reply_link( array_merge( $args, array(
+                    'depth'      => $depth,
+                    'max_depth'  => $args['max_depth'],
+                    'reply_text' => __( 'Reply', MUTHEME_NAME )
+                ) ) ) ?></div>
+        </div>
+    <?php } else {
+        ?>
+    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+        <div id="comment-<?php comment_ID(); ?>" class="comment-body comment-children-body">
+            <div class="comment-avatar"><?php echo get_avatar( $comment, $size = '30' ); ?></div>
+			<span
+                class="comment-floor"><?php comment_reply_link( array_merge( $args, array(
+                    'depth'      => $depth,
+                    'max_depth'  => $args['max_depth'],
+                    'reply_text' => __( 'Reply', JUMPING_NAME )
+                ) ) ) ?></span>
+
+            <div class="comment-data">
+				<span class="comment-span <?php if ( $comment->user_id == 1 ) {
+                    echo "comment-author";
+                } ?>">
+					<?php
+                    $parent_id      = $comment->comment_parent;
+                    $comment_parent = get_comment( $parent_id );
+                    printf( '%s', get_comment_author_link() );
+                    ?>
+				</span>
+				<span
+                    class="comment-span comment-date"><?php echo strtotime($comment->comment_date_gmt); ?></span>
+            </div>
+            <div class="comment-text">
+				<span class="comment-to"><a href="<?php echo "#comment-" . $parent_id; ?>"
+                                            title="<?php echo mb_strimwidth( strip_tags( apply_filters( 'the_content', $comment_parent->comment_content ) ), 0, 100, "..." ); ?>">@<?php echo $comment_parent->comment_author; ?></a>：</span>
+                <?php echo get_comment_text(); ?>
+            </div>
+        </div>
+    <?php }
+}

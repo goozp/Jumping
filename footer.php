33c99c5cfec729a3e0b7280d0d85410deb836bda
gzp <gzp@gzpblog.com>
2017-02-13 01:10:07 +0800

    my分支,统计部分

--- footer.php
+++ footer.php
@@ -47,6 +47,12 @@
         <div class="col-xs-12 col-sm-12 col-lg-12">
             <p class="footer_word">Copyright © 2017 <a id="footer_name" href="<?php bloginfo( 'url' ); ?>" target="_blank"><?php bloginfo( 'name' ); ?></a>
                 All Rights Reserved. Jumping theme powered by <a href="http://www.gzpblog.com" target="_blank">guo</a>.</p>
+            <p>
+                <a href="http://www.miitbeian.gov.cn/">粤ICP备16013442号</a>&nbsp|&nbsp<a href="/sitemap.xml">网站地图</a>&nbsp|&nbsp
+
+                <!-- cnzz统计 -->
+                <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan 		   id='cnzz_stat_icon_1257421693'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1257421693%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+            </p>
         </div>
         <?php
         } ?>
